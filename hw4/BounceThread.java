5c9cde9020bba4607b95e708b52154ff03159a0b
unknown <rjsdn31536@o365.sogang.ac.kr>
2018-11-25 15:53:29 +0900

    aa

--- hw4/BounceThread.java
+++ hw4/BounceThread.java
@@ -2,12 +2,19 @@ package hw4;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class BounceThread extends Frame implements ActionListener {
-	private Canvas canvas;
+	private Canvas_m canvas;
+	private static ArrayList<Ball> ball_list = new ArrayList<Ball>();
+	private static int num_ball = 0;
+	private static Semaphore sema;
 
 	public BounceThread() {
-		canvas = new Canvas();
+		canvas = new Canvas_m();
 		add("Center", canvas);
 		Panel p = new Panel();
 		Button s = new Button("Start");
@@ -17,12 +24,18 @@ public class BounceThread extends Frame implements ActionListener {
 		s.addActionListener(this);
 		c.addActionListener(this);
 		add("South", p);
+		sema = new Semaphore(1);
 	}
 
 	public void actionPerformed(ActionEvent evt) {
 		if (evt.getActionCommand() == "Start") {
-			Ball b1 = new Ball(canvas);
-			b1.start();
+			// �� 5�� �߰�
+			for (int i = 0; i < 5; i++) {
+				ball_list.add(new Ball(canvas, 20));
+				ball_list.get(ball_list.size() - 1).x += 10 * ball_list.get(ball_list.size() - 1).dx;
+				ball_list.get(ball_list.size() - 1).y += 10 * ball_list.get(ball_list.size() - 1).dy;
+				ball_list.get(ball_list.size() - 1).start();
+			}
 		} else if (evt.getActionCommand() == "Close")
 			System.exit(0);
 	}
@@ -30,34 +43,57 @@ public class BounceThread extends Frame implements ActionListener {
 	public static void main(String[] args) {
 		Frame f = new BounceThread();
 		f.setSize(400, 300);
-		WindowDestroyer listener = new WindowDestroyer();
+		WindowDestroyer listener = new WindowDestroyer(); // ������ ���� ��ư
 		f.addWindowListener(listener);
 		f.setVisible(true);
 	}
 
-	private Canvas box;
-	private static final int XSIZE = 20;
-	private static final int YSIZE = 20;
-	private int x = 0;
-	private int y = 0;
-	private int dx = 2;
-	private int dy = 2;
+	public static class WindowDestroyer extends WindowAdapter {
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
 
-	class Ball extends Thread {
-		public Ball(Canvas c) {
-			box = c;
+	// canvas�� ����ؼ� paint�� �������̵��Ͽ� canvas�� ball���� �׸��� �Ͽ���. (�̴�
+	// repaint���� ���δ�)
+	class Canvas_m extends Canvas {
+		public Canvas_m() {
+			super();
 		}
 
-		public void draw() {
-			Graphics g = box.getGraphics();
-			g.fillOval(x, y, XSIZE, YSIZE);
-			g.dispose();
+		public void paint(Graphics g) {
+			g.setColor(Color.BLACK);
+			for (int i = 0; i < ball_list.size(); i++) {
+				if (ball_list.get(i).XSIZE >= 2)
+					g.fillOval(ball_list.get(i).x, ball_list.get(i).y, ball_list.get(i).XSIZE, ball_list.get(i).YSIZE);
+			}
+		}
+	}
+
+	class Ball extends Thread {
+		private Canvas box;
+		public int XSIZE = 20;
+		public int YSIZE = 20;
+		public int x;
+		public int y;
+		public int dx;
+		public int dy;
+
+		public Ball(Canvas c, int size) {
+			num_ball++;
+			box = c;
+			XSIZE = YSIZE = size;
+			x = box.getWidth() / 2;
+			y = box.getHeight() / 2;
+			dx = (int) (Math.random() * 6) - 3;
+			dy = (int) (Math.random() * 6) - 2;
+			if (dx == 0)
+				dx = 3;
+			if (dy == 0)
+				dy = -3;
 		}
 
 		public void move() {
-			Graphics g = box.getGraphics();
-			g.setXORMode(box.getBackground());
-			g.fillOval(x, y, XSIZE, YSIZE);
 			x += dx;
 			y += dy;
 			Dimension d = box.getSize();
@@ -77,16 +113,74 @@ public class BounceThread extends Frame implements ActionListener {
 				y = d.height - YSIZE;
 				dy = -dy;
 			}
-			g.fillOval(x, y, XSIZE, YSIZE);
-			g.dispose();
+		}
+
+		public void collision() {
+			for (int i = 0; i < ball_list.size(); i++) {
+				// �� ���� �Ÿ��� �������� ���� ���� ���ƴ����� Ȯ���Ѵ�.
+				// ���� �������� ball Thread�� 2�� �߰��ϰ�, ũ�⸦ ������ ���δ�.
+				int d_square = ((x + dx) - (ball_list.get(i).x + ball_list.get(i).dx))
+						* ((x + dx) - (ball_list.get(i).x + ball_list.get(i).dx))
+						+ ((y + dy) - (ball_list.get(i).y + ball_list.get(i).dy))
+								* ((y + dy) - (ball_list.get(i).y + ball_list.get(i).dy));
+				int r_square = (XSIZE / 2 + ball_list.get(i).XSIZE / 2) * (XSIZE / 2 + ball_list.get(i).XSIZE / 2);
+				if ((!Ball.currentThread().equals(ball_list.get(i))) && (XSIZE >= 2) && (ball_list.get(i).XSIZE >= 2)
+						&& (d_square < r_square) && (ball_list.size() <= 5050)) {
+					XSIZE /= 2;
+					YSIZE /= 2;
+					dx = -dx;
+					dy = -dy;
+					ball_list.get(i).XSIZE /= 2;
+					ball_list.get(i).YSIZE /= 2;
+					ball_list.get(i).dx = -ball_list.get(i).dx;
+					ball_list.get(i).dy = -ball_list.get(i).dy;
+
+					ball_list.add(new Ball(canvas, XSIZE));
+					ball_list.get(ball_list.size() - 1).x = ((x + ball_list.get(i).x) / 2) + 16 * (-dx);
+					ball_list.get(ball_list.size() - 1).y = ((y + ball_list.get(i).y) / 2)
+							+ 16 * (-ball_list.get(i).dx);
+					ball_list.get(ball_list.size() - 1).dx = -dx;
+					ball_list.get(ball_list.size() - 1).dy = -ball_list.get(i).dx;
+					ball_list.get(ball_list.size() - 1).start();
+
+					ball_list.add(new Ball(canvas, XSIZE));
+					ball_list.get(ball_list.size() - 1).x = ((x + ball_list.get(i).x) / 2)
+							+ 16 * (-ball_list.get(i).dy);
+					ball_list.get(ball_list.size() - 1).y = ((y + ball_list.get(i).y) / 2) + 16 * (-dy);
+					ball_list.get(ball_list.size() - 1).dx = -ball_list.get(i).dy;
+					ball_list.get(ball_list.size() - 1).dy = -dy;
+					ball_list.get(ball_list.size() - 1).start();
+				}
+			}
 		}
 
 		public void run() {
-			draw();
-			for (int i = 1; i <= 1000; i++) {
-				move();
+			// move�� collision�� �����ϰ� �����ð����� ����Ѵ�.
+			// �̶� ����ȭ�� ���־� move�� collision�� �� �����常 ����ϵ��� �Ѵ�.
+			// �׸��� ���� repaint�� ���� �׸��� �׷��ش�.
+			while (true) {
 				try {
-					Thread.sleep(5);
+					sema.acquire();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				move();
+				collision();
+				// ���� ���� ũ�⺸�� �۾����ٸ� �ش� �������� ������ ������.
+				// ���� ���� ��� �浹�� �Ͽ� �������ٸ� �����Ѵ�.
+				if (XSIZE < 2 && YSIZE < 2) {
+					num_ball--;
+					sema.release();
+					if (num_ball == 0)
+						System.exit(0);
+					break;
+				} else {
+					sema.release();
+				}
+				box.repaint();
+				try {
+					Thread.sleep(10);
 				} catch (InterruptedException e) {
 				}
 			}
